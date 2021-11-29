80d2ed2ac8239abb07e7c4f1ee7d5a091c7945ee
Ahmedaljahlani <62110746+Ahmedaljahlani@users.noreply.github.com>
2021-11-29 10:35:22 +0300

    added scrollview & back button,and some features

--- app/src/main/java/com/example/latestrecyclerview/SlopeLogin.java
+++ app/src/main/java/com/example/latestrecyclerview/SlopeLogin.java
@@ -3,14 +3,26 @@ package com.example.latestrecyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
-public class SlopeLogin extends AppCompatActivity {
+public class SlopeLogin extends AppCompatActivity implements View.OnClickListener {
 
+    ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_slope_login);
+
+        mImageView=(ImageView) findViewById(R.id.login_img_back);
+        mImageView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v ==  mImageView)
+            finish();
     }
 }
--- app/src/main/java/com/example/latestrecyclerview/SlopeSignUp.java
+++ app/src/main/java/com/example/latestrecyclerview/SlopeSignUp.java
@@ -5,17 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
-public class SlopeSignUp extends AppCompatActivity {
+public class SlopeSignUp extends AppCompatActivity implements View.OnClickListener {
 
+    ImageView mImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_slope_sign_up);
+
+        mImageView=(ImageView) findViewById(R.id.signup_img_back);
+        mImageView.setOnClickListener(this);
     }
 
-    public void back_arrow(View view) {
 
+    @Override
+    public void onClick(View v) {
+        if (v == mImageView)
+            finish();
     }
 }
